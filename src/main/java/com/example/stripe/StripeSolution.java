298272ad58e23b8b087ecc8bf393a209085962f1
Trever Pietsch <trever.pietsch@hulu.com>
2021-08-24 19:20:46 -0700

    initial commit

---
+++ src/main/java/com/example/stripe/StripeSolution.java
@@ -0,0 +1,7 @@
+package com.example.stripe;
+
+public class StripeSolution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/example/stripe/StripeSolutionTests.java
@@ -0,0 +1,12 @@
+package com.example.stripe;
+
+import org.junit.jupiter.api.Test;
+
+class StripeSolutionTests {
+
+    @Test
+    void test() {
+
+    }
+
+}
