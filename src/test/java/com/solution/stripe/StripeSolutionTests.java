66f8011323b7883e347b3cf930bdafc19dd6a9d4
Trever Pietsch <tpietsch@Trevers-MacBook-Pro-2.local>
2021-08-30 14:14:05 -0700

    adding dummy test

--- src/test/java/com/solution/stripe/StripeSolutionTests.java
+++ src/test/java/com/solution/stripe/StripeSolutionTests.java
@@ -2,11 +2,13 @@ package com.solution.stripe;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class StripeSolutionTests {
 
     @Test
     void test() {
-
+        assertEquals(1, 2);
     }
 
 }
